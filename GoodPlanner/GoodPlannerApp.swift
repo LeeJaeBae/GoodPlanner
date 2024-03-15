065df75b685648bafcabd60b50482ff3e451fd0f
leejaebae <leejaewon0227@gmail.com>
2024-03-15 09:52:01 +0900

    Initial Commit

---
+++ GoodPlanner/GoodPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodPlannerApp.swift
+//  GoodPlanner
+//
+//  Created by 이재원 on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GoodPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
